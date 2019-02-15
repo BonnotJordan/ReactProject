4d79cba8d77ab6fac10ce6edc7183b1c281968f7
Jordan Bonnot <jojobonnot@gmail.com>
2019-02-15 17:40:37 +0100

    finish get result from api

--- src/List.js
+++ src/List.js
@@ -6,7 +6,8 @@ class List extends React.Component {
   
     handleChange(searchText) {
         console.log(searchText.target.value.length)
-        fetch('http://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchText.target.value}&apikey=cc6ba08adc1a5d416f236d8712e9ed31&ts=1597532846&hash=794436f24dd054dead6d8c0950a0dff7')
+        var string = "http://gateway.marvel.com/v1/public/characters?nameStartsWith=" + searchText.target.value + "&apikey=cc6ba08adc1a5d416f236d8712e9ed31&ts=1597532846&hash=794436f24dd054dead6d8c0950a0dff7";
+        fetch(string)
         .then(response => response.json())
             .then(data => {
                 console.log(data)
