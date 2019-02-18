3a7c8dcd6b792f639b5982a71ed8b4b36926e1c7
Jordan Bonnot <jojobonnot@gmail.com>
2019-02-18 15:53:58 +0100

    finish List (WORK)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends React.Component {
   
   constructor(props) {
     super(props);
+    this.state = {
+      selectedButtonId: 0
+    }
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
   }
@@ -21,20 +24,25 @@ class App extends React.Component {
   handleClick(e) {
     e.preventDefault();
     //console.log("Item clicked : " + e.currentTarget.lastChild.data);
-    /*if(e.currentTarget.lastChild.data === "Characters"){
+    if(e.currentTarget.lastChild.data === "Characters"){
       console.log("Vous allez récupérer des Characters");
-      
+      this.state.selectedButtonId = 0
     } else if (e.currentTarget.lastChild.data === "Comics") {
       console.log("Vous allez récupérer des Comics");
+      this.state.selectedButtonId = 1
     } else if (e.currentTarget.lastChild.data === "Creators") {
       console.log("Vous allez récupérer des Creators");
+      this.state.selectedButtonId = 2
     } else if (e.currentTarget.lastChild.data === "Events") {
       console.log("Vous allez récupérer des Events");
+      this.state.selectedButtonId = 3
     } else if (e.currentTarget.lastChild.data === "Series") {
       console.log("Vous allez récupérer des Series");
+      this.state.selectedButtonId = 4
     } else if (e.currentTarget.lastChild.data === "Stories") {
       console.log("Vous allez récupérer des Stories");
-    }*/
+      this.state.selectedButtonId = 5
+    }
   };
 
 
@@ -46,8 +54,16 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p className="help">Search something by tiping it on the search bar or use the list</p>
         </header>
-       <List />
-       <footer>
+       <div className="Buttons">
+       <button type="button" className="button" onClick= {this.handleClick}>Characters</button>
+          <button type="button" className="button" onClick= {this.handleClick}>Comics</button>
+          <button type="button" className="button" onClick= {this.handleClick}>Creators</button>
+          <button type="button" className="button" onClick= {this.handleClick}>Events</button>
+          <button type="button" className="button" onClick= {this.handleClick}>Series</button>
+          <button type="button" className="button" onClick= {this.handleClick}>Stories</button>
+       </div>
+       <List className="itsaList"/>
+       <footer className="App-footer">
            <p className="help">Posted by: Gosse Brandon & Bonnot Jordan</p>
            <p className="help">Github link: <a href="https://github.com/BonnotJordan/ReactProject">
            Github Project Marvel</a>.</p>
--- src/List.js
+++ src/List.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import ListItem from "@material-ui/core/ListItem"
+import Link from '@material-ui/core/Link';
+import ListItemText from '@material-ui/core/ListItemText';
+import './List.css';
 
+var numberOfResult = 0;
+
+function createList(data) {
+  console.log(numberOfResult)
+  var all = [];
+for(var i=0;i<numberOfResult;i++){
+  console.log(i)
+  console.log(data.data.results[i].name)
+  const element = <ListItem button><ListItemText primary={data.data.results[i].name}></ListItemText></ListItem>;
+  all.push(element)
+}
+ReactDOM.render(all, document.getElementById('List'));
+}
 
 function loadAllCharacters()  {
+  
   var urlDefault = "http://gateway.marvel.com/v1/public/characters?apikey=cc6ba08adc1a5d416f236d8712e9ed31&ts=1597532846&hash=794436f24dd054dead6d8c0950a0dff7";
+  
   fetch(urlDefault)
   .then(response => response.json())
       .then(data => {
           console.log(data)
-          console.log()
+          numberOfResult = data.data.count
+          createList(data)
   })
   .catch(err => console.error(this.props.url, err.toString()))
+  
 }
 
 class List extends React.Component {
@@ -27,13 +49,15 @@ class List extends React.Component {
         fetch(uRLWithName)
         .then(response => response.json())
             .then(data => {
+                numberOfResult = data.data.count
                 console.log(data)
                 console.log()
+                createList(data)
         })
         .catch(err => console.error(this.props.url, err.toString()))
       }
     }
-
+    
     
 
     render() {
@@ -43,8 +67,10 @@ class List extends React.Component {
         <div className="Search">
             <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />                    
         </div>
-        <div className="List">
-                                
+        <div id="List">
+        
+
+                        
         </div>
             
         </div>
